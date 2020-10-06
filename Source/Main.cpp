f47c96e04dff9b6732c6fba8385e71b84acc93d1
thomas-dumonet <thomas.dumonet@gmail.com>
2020-10-06 17:44:32 +0200

    native build option

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -18,46 +18,11 @@ public:
     const String getApplicationName() override       { return "StepSequencerDemo"; }
     const String getApplicationVersion() override    { return "0.0.1"; }
 
-    void initialise (const String&) override         { mainWindow.reset (new MainWindow ("StepSequencerDemo", new StepSequencerDemo(), *this)); }
-    void shutdown() override                         { mainWindow = nullptr; }
+    void initialise (const String&) override         { new StepSequencerDemo(); }
+    void shutdown() override                         {  }
 
 private:
-    class MainWindow    : public DocumentWindow
-    {
-    public:
-        MainWindow (const String& name, Component* c, JUCEApplication& a)
-            : DocumentWindow (name, Desktop::getInstance().getDefaultLookAndFeel()
-                                                          .findColour (ResizableWindow::backgroundColourId),
-                              DocumentWindow::allButtons),
-              app (a)
-        {
-            setUsingNativeTitleBar (true);
-            setContentOwned (c, true);
 
-           #if JUCE_ANDROID || JUCE_IOS
-            setFullScreen (true);
-           #else
-            setResizable (true, false);
-            setResizeLimits (300, 250, 10000, 10000);
-            centreWithSize (getWidth(), getHeight());
-           #endif
-
-            setVisible (true);
-        }
-
-        void closeButtonPressed() override
-        {
-            app.systemRequestedQuit();
-        }
-
-    private:
-        JUCEApplication& app;
-
-        //==============================================================================
-        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainWindow)
-    };
-
-    std::unique_ptr<MainWindow> mainWindow;
 };
 
 //==============================================================================
